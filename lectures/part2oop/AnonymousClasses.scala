14ecdd8ab0aa6e17a0127b9e03572864d025f72f
Hazel Martinez <hazel@MacBook-Pro-de-Hazel.local>
2020-12-08 02:36:39 -0600

    Generic types

---
+++ lectures/part2oop/AnonymousClasses.scala
@@ -0,0 +1,45 @@
+package lectures.part2oop
+
+object AnonymousClasses extends App {
+
+  abstract class Animal {
+    def eat: Unit
+  }
+
+
+  // anonymous class
+  val funnyAnimal: Animal = new Animal {
+    override def eat: Unit = println("ahahahahaha")
+  }
+  /* equivalent with
+    class AnonymousClasses$$anon$1 extends Animal {
+    override def eat: Unit = println("ahahaha")
+  }
+  val funnyAnimal: Animal = new AnonymousClasses$$anon$
+   */
+
+  println(funnyAnimal.getClass)
+
+  class Person (name: String) {
+    def sayHi: Unit = println(s"Hi, my name is $name, how ca I help")
+  }
+
+  val jim = new Person("Jim") {
+    override def sayHi: Unit = println(s"Hi, my name is Jim, how can I be of service?");
+  }
+
+  /*
+  1. Generic trait MyPredicate[T] with a littler method test (T) => Boolean
+  2. Generic trait MyTransformer[A, B] with a method transform(A) => B
+  3. My List:
+    - map(transformer) => MyList
+    - filter(predicate) => MyList
+    - flatMap(transformer from a to MyList[B]) => MyList[B]
+
+  class EvenPredicate extends MyPredicate[Int]
+  class StringIntTransformer extends MyTransformer[String, Int]
+  [1,2,3,].map (n * 2) = [2,4,6]
+  [1,2,3,4].filter (n % 2) = [2,4]
+  [1,2,3].flatMap(n => [n, n+1] ) => [1,2,3,4]
+   */
+}
